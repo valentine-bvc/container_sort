dd6fcc4803f594edafa1b390b1639b2e064a6fd7
Valentin Bruma <valentine.bvc@gmail.com>
2023-02-27 08:50:40 +0100

    add initial commit

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+
+
+template<typename T>
+void print_collection(T collection [], size_t size) {
+    for(size_t i {}; i < size; i++)
+        std::cout << collection[i] << " ";
+    
+    std::cout << std::endl;
+}
+
+template<typename T>
+void sort_it(T collection [],size_t size){
+    for(size_t i{1}; i < size; i++){
+        T key=collection[i];
+        int j=i-1;
+        while(j>=0 && collection[j] > key){
+            collection[j+1] = collection[j];
+            collection[j]=key;
+            j--;
+        } 
+        
+        collection[j+1]=key;
+        
+    }
+    
+    
+} 
+
+/// Solution without templates
+///
+//void print_collection(auto collection [], size_t size) {
+//    for(size_t i {}; i < size; i++)
+//        std::cout << collection[i] << " ";
+//    
+//    std::cout << std::endl;
+//}
+//
+//
+//void sort_it(auto collection [],size_t size){
+//    for(size_t i{1}; i < size; i++){
+//        auto key=collection[i];
+//        int j=i-1;
+//        while(j>=0 && collection[j] > key){
+//            collection[j+1] = collection[j];
+//            collection[j]=key;
+//            j--;
+//        } 
+//        
+//        collection[j+1]=key;
+//        
+//    }
+//    
+//    
+//}
+
+
+
+
+int main()
+{
+    
+///// testing examples
+   std::string names[] {"Zack", "Courtney", "Shaley", "Andrew", "Sun", "Vick"};
+   
+   std::cout <<  "Before sorting: " << std::endl;;
+   print_collection(names, std::size(names));
+   
+   sort_it(names, std::size(names));
+   
+   std::cout <<  "\nAfter sorting: " << std::endl;;
+   print_collection(names, std::size(names));
+   
+   std::cout << std::endl << std::endl;
+   
+   int numbers [] {20, 2, 40 , 3 , 1 , 19, 50, 14};
+   
+   std::cout <<  "Before sorting: " << std::endl;;
+   print_collection(numbers, std::size(names));
+   
+   sort_it(numbers, std::size(names));
+   
+   std::cout <<  "\nAfter sorting: " << std::endl;;
+   print_collection(numbers, std::size(names));
+   
+   std::cout << std::endl << std::endl;
+   
+    return 0;
+}
